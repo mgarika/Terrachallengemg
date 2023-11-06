554095cd13f30d7a62099e45f42770b32b2f7469
mgarika <monica.garika@gmail.com>
2023-11-06 00:40:31 -0500

    Windows commit

--- main.tf
+++ main.tf
@@ -110,12 +110,11 @@ resource "azurerm_network_interface" "vm3-nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.testmg-subnet3.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.testmg_public_ip.id
+    
   }
 
   depends_on = [ 
-     azurerm_virtual_network.testmg-vn,
-     azurerm_public_ip.testmg_public_ip
+     azurerm_virtual_network.testmg-vn
    ]
 }
 resource "azurerm_windows_virtual_machine" "Jumpbox-VM" {
